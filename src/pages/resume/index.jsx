939a5f592b035631cd7361c357e5f969e0ba1875
Gianfranco Marzulli <marzulligianfranco@gmail.com>
2023-07-19 16:06:53 +0200

    added resume page items

--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import styles from "./index.module.css";
 
 const Resume = () => {
   return (
-    <div>Resume</div>
-  )
-}
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        Unlocking the Potential of MERN: Transforming Ideas into Seamless Web
+        Solutions
+      </h1>
+      <h2 className={styles.subtitle}>
+        Are you ready to unleash the full potential of your online presence?
+        Look no further!
+      </h2>
+      <h2 className={styles.presentation}>
+        Welcome to my world of MERN full-stack development, where I combine
+        technical finesse with a touch of creative magic. As a skilled and
+        dedicated developer, I specialize in crafting sophisticated web
+        solutions using the power of MongoDB, Express.js, React.js, and Node.js.
+      </h2>
+      <h2 className={styles.presentation}>
+        Now, you may be thinking, "But wait, how can someone with independent
+        practices make a difference?" Well, here's the secret sauce: my
+        independent projects have been my playground for innovation and
+        learning. They've given me the freedom to explore new horizons, adapt
+        quickly, and find creative solutions to challenges that come my way.
+      </h2>
+      <h2 className={styles.presentation}>
+        But what truly sets me apart is my ability to connect with you on a
+        personal level. I understand that behind every website or application
+        lies a vision, a dream, a spark of inspiration. That's why I take the
+        time to listen, to understand your unique goals and aspirations.
+        Together, we'll collaborate closely to ensure that every line of code I
+        write aligns with your vision and resonates with your audience.
+      </h2>
+      <h2 className={styles.presentation}>
+        When you choose to work with me, you're not just getting a MERN expert.
+        You're joining forces with a passionate partner who believes in open
+        communication, integrity, and delivering results that go beyond
+        expectations. Together, we'll embark on an exciting journey, turning
+        your ideas into a seamless digital experience that captivates and
+        delights.
+      </h2>
+      <h2 className={styles.presentation}>
+        Whether you're a forward-thinking entrepreneur or an established
+        business seeking a fresh perspective, let's join forces and unlock the
+        full potential of your online presence. Reach out today, and let's
+        create something extraordinary together.
+      </h2>
+    </div>
+  );
+};
 
-export default Resume
+export default Resume;
