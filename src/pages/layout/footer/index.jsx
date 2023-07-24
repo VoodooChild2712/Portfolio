3cdab48c4aacda4e17be5cf354f4f22a39b747b0
Gianfranco Marzulli <marzulligianfranco@gmail.com>
2023-07-24 19:22:38 +0200

    added footer content

--- src/pages/layout/footer/index.jsx
+++ src/pages/layout/footer/index.jsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import styles from "../index.module.css"
+import React from "react";
+import styles from "../index.module.css";
 
 const Footer = () => {
   return (
-    <div className={styles.footer}>Footer</div>
-  )
-}
+    <div className={styles.footer}>
+      <h1 className={styles.footerTitle}>
+        Gianfranco {"<"}/{">"} Marzulli
+      </h1>
+      <h2 className={styles.footerText}>
+        Thanks for taking a look!
+      </h2>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
